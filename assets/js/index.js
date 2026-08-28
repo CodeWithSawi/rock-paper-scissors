25e118ded9216f48478f265070a9421c8e247ea3
Slim Shady <momohsawi5@gmail.com>
2026-08-28 12:58:23 +0000

    feat: refine playRound function and also update scores

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,6 +24,8 @@ let result = document.getElementById("result");
 let rock = document.getElementById("rock");
 let paper = document.getElementById("paper");
 let scissors = document.getElementById("scissors");
+let userEl = document.getElementById("user");
+let compEl = document.getElementById("computer");
 
 // declare global variables
 let userScore = 0;
@@ -33,10 +35,15 @@ const choices = ["rock", "paper", "scissors"];
 // now i need to get the random index of the choices so that the cp will be able to choose
 const getComputerChoice = () => {
   let randomIndex = Math.floor(Math.random() * choices.length);
-  return +choices[randomIndex];
+  return choices[randomIndex];
 };
 
-const playRound = () => {
+const updateScores = () => {
+  userEl.textContent = `User Score: ${userScore}`;
+  compEl.textContent = `Computer Score: ${computerScore}`;
+};
+
+const playRound = (userChoice) => {
   const computerChoice = getComputerChoice();
   if (computerChoice === userChoice) {
     result.textContent = `It's a tie! You both chose ${userChoice}.`;
@@ -46,9 +53,23 @@ const playRound = () => {
     (userChoice === "scissors" && computerChoice === "paper")
   ) {
     userScore++;
+
     result.textContent = `You win! ${userChoice} beats ${computerChoice}.`;
   } else {
     computerScore++;
     result.textContent = `You lose! ${computerChoice} beats ${userChoice}.`;
   }
+  updateScores();
 };
+
+rock.addEventListener("click", function () {
+  playRound("rock");
+});
+
+paper.addEventListener("click", function () {
+  playRound("paper");
+});
+
+scissors.addEventListener("click", function () {
+  playRound("scissors");
+});
